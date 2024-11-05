1d388dc97d97468d760b45def1a623e44bf7ea45
Prasad Hadshi <162487943+prasadhadashi@users.noreply.github.com>
2024-11-05 21:35:41 +0530

    simple Project
    portfolio

---
+++ main.js
@@ -0,0 +1,8 @@
+let typed = new Typed(".text", {
+    strings:["Frontend Developer" , "Web Developer" , "Learner"],
+    typeSpeed: 100,
+    backSpeed: 100,
+    backDelay: 1000,
+    loop:true
+});
+
